e58b3c0de6ffa3044e52fe10287ed427d09bcdde
meshalalmoqdadassistio <meshal.almoqdad@assistio.co>
2026-04-09 12:19:45 +0300

    Publish official Assistio website for ai.assistio.co

---
+++ ASSISTIO OFFICIAL WEBSITE/site-config.js
@@ -0,0 +1,94 @@
+window.ASSISTIO_SITE_CONFIG = Object.freeze({
+  siteUrl: "https://ai.assistio.co",
+  bookingUrl: "https://calendar.app.google/mF4N138gHQZpTKqc6",
+  contactFormEndpoint: "https://formspree.io/f/xqekolgy",
+  localeVersion: "2026-04-08-b",
+  localePaths: Object.freeze({
+    en: "./locales/en.json",
+    ro: "./locales/ro.json",
+    ar: "./locales/ar.json"
+  }),
+  company: Object.freeze({
+    brandName: "Assistio",
+    legalEntityName: "Assistio",
+    supportEmail: "meshal.almoqdad@assistio.co",
+    privacyEmail: "meshal.almoqdad@assistio.co",
+    legalEmail: "meshal.almoqdad@assistio.co",
+    registeredAddress: Object.freeze({
+      en: "Available upon request or in a signed commercial agreement.",
+      ro: "Disponibil\u0103 la cerere sau \u00eentr-un acord comercial semnat.",
+      ar: "\u0645\u062a\u0627\u062d \u0639\u0646\u062f \u0627\u0644\u0637\u0644\u0628 \u0623\u0648 \u0636\u0645\u0646 \u0627\u062a\u0641\u0627\u0642 \u062a\u062c\u0627\u0631\u064a \u0645\u0648\u0642\u0651\u0639."
+    }),
+    governingLaw: Object.freeze({
+      en: "The law stated in the final signed agreement or other applicable mandatory law.",
+      ro: "Legea indicat\u0103 \u00een acordul final semnat sau orice alt\u0103 lege obligatorie aplicabil\u0103.",
+      ar: "\u0627\u0644\u0642\u0627\u0646\u0648\u0646 \u0627\u0644\u0645\u062d\u062f\u062f \u0641\u064a \u0627\u0644\u0627\u062a\u0641\u0627\u0642 \u0627\u0644\u0646\u0647\u0627\u0626\u064a \u0627\u0644\u0645\u0648\u0642\u0651\u0639 \u0623\u0648 \u0623\u064a \u0642\u0627\u0646\u0648\u0646 \u0625\u0644\u0632\u0627\u0645\u064a \u0648\u0627\u062c\u0628 \u0627\u0644\u062a\u0637\u0628\u064a\u0642."
+    }),
+    effectiveDate: Object.freeze({
+      en: "April 7, 2026",
+      ro: "7 aprilie 2026",
+      ar: "7 \u0623\u0628\u0631\u064a\u0644 2026"
+    })
+  }),
+  contacts: Object.freeze([
+    Object.freeze({
+      name: "Meshal Almoqdad",
+      roleKey: "contact.teamRole",
+      phoneLabel: "+40 0729 501 787",
+      phoneHref: "tel:+400729501787",
+      email: "meshal.almoqdad@assistio.co"
+    }),
+    Object.freeze({
+      name: "Ali Annamuhammedov",
+      roleKey: "contact.teamRole",
+      phoneLabel: "+86 159 2122 1261",
+      phoneHref: "tel:+8615921221261",
+      email: "ali.annamuhammedov@assistio.co"
+    })
+  ]),
+  socialLinks: Object.freeze([
+    Object.freeze({
+      id: "whatsapp",
+      href: "https://wa.me/400729501787"
+    }),
+    Object.freeze({
+      id: "email",
+      href: "mailto:meshal.almoqdad@assistio.co"
+    }),
+    Object.freeze({
+      id: "x",
+      href: "https://x.com/Assistio_io"
+    }),
+    Object.freeze({
+      id: "facebook",
+      href: "https://www.facebook.com/profile.php?id=61585236956670"
+    }),
+    Object.freeze({
+      id: "instagram",
+      href: "https://www.instagram.com/assistio.io/"
+    }),
+    Object.freeze({
+      id: "tiktok",
+      href: "https://www.tiktok.com/@assistio.io"
+    }),
+    Object.freeze({
+      id: "bookDemo",
+      href: "https://calendar.app.google/mF4N138gHQZpTKqc6"
+    }),
+    Object.freeze({
+      id: "website",
+      href: "https://ai.assistio.co"
+    }),
+    Object.freeze({
+      id: "linkedin",
+      href: ""
+    })
+  ]),
+  chatwoot: Object.freeze({
+    enabled: true,
+    baseUrl: "https://chat.assistio.co",
+    websiteToken: "A9a8QDS4WY1BF6SRKySLWm2u",
+    position: "right",
+    hideMessageBubble: false
+  })
+});
